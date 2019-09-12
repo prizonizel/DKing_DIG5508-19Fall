48df16b6a82b54a3e68138a6addec3edf7ba69b6
prizonizel <islanddaniel@hotmail.com>
2019-09-11 20:44:10 -0400

    So there's a source control tab, it turns out

--- lab0/hello_world.py
+++ lab0/hello_world.py
@@ -1,3 +1,4 @@
 print("Hooray for Python and Git")
 print("Man, this stuff is complicated though")
 print("But hey, at least I figured out commit, push, and pull")
+print("Oh, there's a source control tab, lol")
