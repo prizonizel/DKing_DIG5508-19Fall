c208cdf9993f3a89e6c5937da45e6c3303d222c2
prizonizel <54692098+prizonizel@users.noreply.github.com>
2019-09-25 19:54:22 -0400

    made increment a function

--- plusone.py
+++ plusone.py
@@ -1,7 +1,6 @@
 def plusone(x):
     return x+1
 
-increment = 0
-increment = plusone(increment)
-increment = plusone(increment)
-increment = plusone(increment)
+def increment(x):
+    return plusone(plusone(plusone(x)))
+
